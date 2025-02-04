0eac383e021094f5b3fc8a8f151a1eb57da01027
gr426_reepe <repel.egor@gmail.com>
2025-02-04 14:00:59 +0700

    pnsk3

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ let app = new Vue({
         altText: "A pair of socks",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks.",
         inStock: true,
-        inventory: 100,
+        inventory: 0,
         onsale:  "On Sale",
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
