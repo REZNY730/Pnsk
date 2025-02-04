a43be23cbf09b97a7848f4e655289a8098feff21
gr426_reepe <repel.egor@gmail.com>
2025-02-04 13:46:57 +0700

    pnsk3

--- js/main.js
+++ js/main.js
@@ -6,9 +6,36 @@ let app = new Vue({
         image: "./assets/vmSocks-blue-onWhite.jpg",
         altText: "A pair of socks",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks.",
-        inStock: false,
+        inStock: true,
         inventory: 100,
         onsale:  "On Sale",
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green',
+                variantImage: "./assets/vmSocks-green-onWhite.jpg",
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue',
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+            }
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+        cart: 0,
+        updateProduct(variantImage) {
+            this.image = variantImage
+        },
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        deletToCart() {
+            if (this.cart > 0)
+            this.cart -= 1
+        }
     }
 })
 
