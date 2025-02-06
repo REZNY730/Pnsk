dcda102e3f6185aa5293c022cc5afdebb51878b2
gr426_reepe <repel.egor@gmail.com>
2025-02-06 09:32:25 +0700

    pnsk5

--- js/main.js
+++ js/main.js
@@ -1,14 +1,6 @@
 Vue.component('product-review', {
     template: `
-     <form class="review-form" @submit.prevent="onSubmit">
-
-<p v-if="errors.length">
- <b>Please correct the following error(s):</b>
- <ul>
-   <li v-for="error in errors">{{ error }}</li>
- </ul>
-</p>
-
+<form class="review-form" @submit.prevent="onSubmit">
  <p>
    <label for="name">Name:</label>
    <input id="name" v-model="name" placeholder="name">
@@ -28,38 +20,52 @@ Vue.component('product-review', {
      <option>2</option>
      <option>1</option>
    </select>
+   <p>
+<label>Would you recommend this product?</label>
+    <label>
+        <input type="radio" value="yes" v-model="recommendation"> Yes
+    </label>
+    <label>
+        <input type="radio" value="no" v-model="recommendation"> No
+    </label>
+    </p>
  </p>
-
  <p>
    <input type="submit" value="Submit"> 
  </p>
 
 </form>
+
  `,
     data() {
         return {
             name: null,
             review: null,
             rating: null,
-            error: null
+            recommendation: null,
+            errors: []
         }
     },
         methods:{
             onSubmit() {
+                this.errors = [];
                 if(this.name && this.review && this.rating) {
                     let productReview = {
                         name: this.name,
                         review: this.review,
-                        rating: this.rating
+                        rating: this.rating,
+                        recommendation: this.recommendation
                     }
                     this.$emit('review-submitted', productReview)
                     this.name = null
                     this.review = null
                     this.rating = null
+                    this.recommendation = null
                 } else {
                     if(!this.name) this.errors.push("Name required.")
                     if(!this.review) this.errors.push("Review required.")
                     if(!this.rating) this.errors.push("Rating required.")
+                    if (!this.recommendation) this.errors.push("Recommendation required.")
                 }
             }
         }
@@ -108,6 +114,7 @@ Vue.component('product', {
   <p>{{ review.name }}</p>
   <p>Rating: {{ review.rating }}</p>
   <p>{{ review.review }}</p>
+  <p>Recommendation: {{ review.recommendation }}</p>
   </li>
 </ul>
 </div> <product-review @review-submitted="addReview"></product-review>
